e164b9dacdf21707765c4166e9d387c2718ee282
Khang <khang.nguyen.trd@gmail.com>
2022-02-23 09:17:20 -0600

    Initial commit.

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Nguyen Tran Duy Khang on 2/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
